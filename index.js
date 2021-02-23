02a31528f1af1480b11ff661642a1731e2ff812d
jianminzhu <jianminzhu@ctrip.com>
2021-02-23 10:48:42 +0800

    添加命令

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
 const { program } = require('commander')
 const helpOptions = require('./lib/core/help')
-const fs = require('fs')
-const path = require('path')
-const { useTypeAction } = require('./lib/core/actions')
 const generateConfig = require('./lib/core/generateConfig')
 const pkg = require('./package.json')
+const { configCommand } = require('./lib/core/command')
 
 helpOptions()
+configCommand()
 
 program
     .version(pkg.version)
     .parse(process.argv);
+
 
 const config = generateConfig()
 const { type } = program.opts()
 if(['github','ali-oss','qiniu','ftp'].indexOf(type) > -1){
     require(`./lib/${type}`)(config,program.args)
 }
-console.log(config)
-console.log(program.args)
